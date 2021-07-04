6606e377ca36feb09cf1c538fa6f3049c199bbfd
Damixiorek <damixiorek@gmail.com>
2021-07-04 18:20:22 +0200

    fix cout info in Copy Full Class

--- CopyFull.hpp
+++ CopyFull.hpp
@@ -5,25 +5,25 @@
 class CopyFull {
 public:
 	CopyFull(const CopyFull& anotherObject) {
-		std::cout << "Do something in Copy Constructor\n";
+		std::cout << "Do something in Copy Full Constructor\n";
 	}
 
 	CopyFull& operator=(const CopyFull& anotherObject) {
 		if(this != &anotherObject){
-			std::cout << "Do something in Copy Assignment\n";
+			std::cout << "Do something in Copy Full Assignment\n";
 		}
 		return *this;
 	}
 
 	std::string getName() const {
-		return "Copy Constructor Declared Class";
+		return "Copy Full Declared Class";
 	}
 
 	friend CopyFull* createCF();
 
 private:
 	CopyFull(){
-		std::cout << "Do something in Private \"Default\" Constructor in Copy Constructor Class\n";
+		std::cout << "Do something in Private \"Default\" Constructor in Copy Full Class\n";
 	}
 };
 
