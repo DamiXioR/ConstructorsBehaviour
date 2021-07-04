4a226020218dbf5d9b8b1efecd886fc448ab65d7
Damixiorek <damixiorek@gmail.com>
2021-07-04 11:58:53 +0200

    add NothingDeclared

---
+++ NothingDeclared.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+class NothingDeclared {
+public:
+	std::string getName() const {
+		return "Nothing Declared Class";
+	}
+};
--- main.cpp
+++ main.cpp
@@ -1,5 +1,52 @@
+#include <iostream>
+#include <utility>
+#include <type_traits>
 
+#include "NothingDeclared.hpp"
+
+template <class T>
+void check(T& object) {
+	
+	if(std::is_default_constructible<T>::value){
+		std::cout << object.getName() << " is default constructible\n";
+	} else {
+		std::cout << object.getName() << " is not default constructible\n";
+	}
+
+
+	if(std::is_copy_constructible<T>::value){
+		std::cout << object.getName() << " is copy constructible\n";
+	} else {
+		std::cout << object.getName() << " is not copy constructible\n";
+	}	
+
+	if(std::is_copy_assignable<T>::value){
+		std::cout << object.getName() << " is copy assignable\n";
+	} else {
+		std::cout << object.getName() << " is not copy assignable\n";
+	}	
+
+
+	if(std::is_move_constructible<T>::value){
+		std::cout << object.getName() << " is move constructible\n";
+	} else {
+		std::cout << object.getName() << " is not move constructible\n";
+	}	
+
+	if(std::is_move_assignable<T>::value){
+		std::cout << object.getName() << " is move assignable\n";
+	} else {
+		std::cout << object.getName() << " is not move assignable\n";
+	}	
+
+}
+	
 int main(){
+	//NOTHING DECLARED BY PROGRAMMER
+	
+	NothingDeclared nD;
+	check(nD);
+
 	return 0;
 }
 
